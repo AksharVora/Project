7bc865af6603ef3c6f628a7e31249aa4e6c04ebb
unknown <aksharvora11@gmail.com>
2022-01-18 20:11:34 +0530

    Autus HTML

--- Autus/assets/js/scripts.js
+++ Autus/assets/js/scripts.js
@@ -189,6 +189,32 @@ function testimonialSliders(){
 	  infinite: true,
 	  autoplay: true,
   	autoplaySpeed: 4000,
-
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
 	});
 }
