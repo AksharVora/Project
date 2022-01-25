e05ef700a1072db9d1cfee925fd79602d2f82926
unknown <aksharvora11@gmail.com>
2022-01-25 20:02:55 +0530

    Autus HTML

--- purple-moon/assets/js/scripts.js
+++ purple-moon/assets/js/scripts.js
@@ -2,6 +2,35 @@ $(document).ready(function(){
 	serviceSlider();
   videoSlider()
 });
+$(document).ready(function() {
+  $(".set > a").on("click", function(event) {
+    event.preventDefault();
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $(".set > a i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > a").removeClass("active");
+      $(this).addClass("active");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
+    }
+  });
+});
+
 $(window).on("load" , function(){
   
   $(window).resize(function(){
