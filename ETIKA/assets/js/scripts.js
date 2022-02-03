8ce6a6825358b214b301dd2319e93a8943853ba5
unknown <aksharvora11@gmail.com>
2022-02-03 21:06:39 +0530

    ETIKA HTML

--- ETIKA/assets/js/scripts.js
+++ ETIKA/assets/js/scripts.js
@@ -83,6 +83,49 @@ $(document).ready(function(){
 	  ]
 	});
 
+	$('.gallery-slider').slick({
+	  
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  centerMode: true,
+	  arrows: false,
+
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1,
+	        infinite: true,
+	        dots: true
+	      }
+	    },
+	    {
+	      breakpoint: 767,
+	      settings: {
+	      	centerMode: false,
+	      	infinite: true,
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	      	centerMode: false,
+	      	infinite: true,
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+	});
+
 
 	$('.testimonial-slider').slick({
 	  
